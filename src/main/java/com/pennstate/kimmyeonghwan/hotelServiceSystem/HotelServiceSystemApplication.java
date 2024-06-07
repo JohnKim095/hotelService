c94e4f052c7f8ee556f32782c4aff971fc4a7820
John Kim <kimmh0577@gmail.com>
2024-06-06 21:27:22 -0500

    initial commit

---
+++ src/main/java/com/pennstate/kimmyeonghwan/hotelServiceSystem/HotelServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pennstate.kimmyeonghwan.hotelServiceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceSystemApplication.class, args);
+	}
+
+}
